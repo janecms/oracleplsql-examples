6f1ff579b83b7debe86ce724064bb19838c4546c
janecms <randy_shandong@163.com>
2014-10-10 13:21:41 +0800

    Update declare_proc.sql

--- chap02_lang-fundamental/declare_proc.sql
+++ chap02_lang-fundamental/declare_proc.sql
@@ -4,10 +4,10 @@ DECLARE
    in_string VARCHAR2(100) := 'This is my test string.';
    out_string VARCHAR2(200);
  
-   PROCEDURE double ( original IN VARCHAR2, new_string OUT VARCHAR2 ) AS
+   PROCEDURE double ( original IN VARCHAR2, new_string OUT VARCHAR2 ) AS -- 可以是 is
    BEGIN
          new_string := original || original;
-   END;
+   END;--可以是end double
 BEGIN
  
    double(in_string, out_string);
